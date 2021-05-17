74e6fc8eead82b0c8d39e0423aa2bbc8efa11158
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-17 16:43:43 -0500

    create twoSum tests

--- two_sum/test.js
+++ two_sum/test.js
@@ -1,2 +1,21 @@
 const twoSum = require("./index.js");
 
+test('twoSum function exists', () => {
+  expect(typeof twoSum).toEqual('function');
+});
+
+test('outputs [0, 2]', () => {
+  expect(twoSum([3, 5, 6, 10], 9)).toEqual([0, 2]);
+});
+
+test('outputs [0, 1]', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+});
+
+test('outputs [1, 2]', () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
+});
+
+test('outputs [0, 1]', () => {
+  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
+});
