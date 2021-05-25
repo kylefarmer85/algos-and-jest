871a22578dd0a7cfa99e981d2ee1d016ed2faf9c
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-25 09:42:34 -0500

    create mumbling index and test

---
+++ mumbling/test.js
@@ -0,0 +1,13 @@
+const accum = require('./index');
+
+test('outputs A-Bb-Ccc-Dddd', () => {
+  expect(accum('abcd')).toBe('A-Bb-Ccc-Dddd');
+});
+
+test('outputs R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy', () => {
+  expect(accum('RqaEzty')).toBe('R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy');
+});
+
+test('outputs C-Ww-Aaa-Tttt', () => {
+  expect(accum('cwAt')).toBe('C-Ww-Aaa-Tttt');
+});
