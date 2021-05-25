24713a2620e6a2099c4f86c3a3f0769311f445f8
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-25 10:40:54 -0500

    new solution for repeated chars

--- mumbling/index.js
+++ mumbling/index.js
@@ -8,14 +8,33 @@
 
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
+// function accum(s) {
+//   let result = ''
+//   for (let char of s) {
+//     let repeatedChar = char.repeat(s.indexOf(char) + 1);
+//     if (s.indexOf(char) === s.length - 1) {
+//       result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1)
+//     } else {
+//       result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1) + "-"
+//     }
+//   }
+//   return result;
+// }
+
+
+// new solution for repeated chars accum('aABcb')
 function accum(s) {
-  let result = ''
-  for (let char of s) {
-    let repeatedChar = char.repeat(s.indexOf(char) + 1);
-    if (s.indexOf(char) === s.length - 1) {
-      result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1)
+  let result = '';
+  for (let i = 0; i <= s.length - 1; i++) {
+    let repeatedChar = s[i].repeat(i + 1);
+    if (i === s.length - 1) {
+      result +=
+        repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1);
     } else {
-      result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1) + "-"
+      result +=
+        repeatedChar[0].toUpperCase() +
+        repeatedChar.toLowerCase().slice(1) +
+        '-';
     }
   }
   return result;
--- mumbling/test.js
+++ mumbling/test.js
@@ -11,3 +11,7 @@ test('outputs R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy', () => {
 test('outputs C-Ww-Aaa-Tttt', () => {
   expect(accum('cwAt')).toBe('C-Ww-Aaa-Tttt');
 });
+
+test('output A-Aa-Bbb-Cccc-Bbbbb', () => {
+  expect(accum('aABcb')).toBe('A-Aa-Bbb-Cccc-Bbbbb');
+});
