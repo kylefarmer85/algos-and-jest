92f27fe6a8462487287bb397074ab28600b7d186
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-25 10:23:40 -0500

    solve mumbling

--- mumbling/index.js
+++ mumbling/index.js
@@ -9,7 +9,16 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 function accum(s) {
-
+  let result = ''
+  for (let char of s) {
+    let repeatedChar = char.repeat(s.indexOf(char) + 1);
+    if (s.indexOf(char) === s.length - 1) {
+      result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1)
+    } else {
+      result += repeatedChar[0].toUpperCase() + repeatedChar.toLowerCase().slice(1) + "-"
+    }
+  }
+  return result;
 }
 
 module.exports = accum;
