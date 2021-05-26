0d65b9df190f2429d022dfd3055b8fd4e4d1c938
kylefarmer85 <kylefarmer85@gmail.com>
2021-05-26 09:40:29 -0500

    alter binary search function

--- binary_search/index.js
+++ binary_search/index.js
@@ -1,21 +1,20 @@
 // Return the index of num if it is in the sorted array. If not, return -1.
 
 function binarySearch(arr, num) {
-  let startIndex = 0;
-  let endIndex = arr.length - 1;
+  let start = 0;
+  let end = arr.length - 1;
 
-  while(startIndex <= endIndex) {
-    let midIndex = Math.floor(endIndex - startIndex / 2);
-    let midValue = arr[midIndex];
+  while(start <= end) {
+    let mid = Math.floor((start + end) / 2);
 
-    if (midValue === num) {
-      return midIndex;
+    if (arr[mid] === num) {
+      return mid;
     }
-    if (num > midValue) {
-      startIndex = midIndex + 1;
+    if (num > arr[mid]) {
+      start = mid + 1;
     }
-    if (num < midValue) {
-      endIndex = midIndex - 1;
+    else {
+      end = mid - 1;
     }
   }
   return - 1
