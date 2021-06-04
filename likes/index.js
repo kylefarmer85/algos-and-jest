0e0914a61b5958cf462386b33bf144e642998db9
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-03 19:53:06 -0500

    likes solution

--- likes/index.js
+++ likes/index.js
@@ -10,7 +10,18 @@
 // For 4 or more names, the number in and 2 others simply increases.
 
 function likes(names) {
-
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+    case 1:
+      return `${names[0]} likes this`;  
+    case 2:
+      return `${names[0]} and ${names[1]} like this`  
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`  
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+  }
 }
 
 module.exports = likes;
