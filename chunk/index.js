0da9c0ff8f558e62438f08c0506552740c32a1c5
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-03 20:10:04 -0500

    create chunk index and test

---
+++ chunk/index.js
@@ -0,0 +1,10 @@
+// Given an array and chunk size, divide the array into many subarrays where each subarray is of the provided size.
+
+// Examples:
+// chunk([1, 2, 3, 4], 2) --> [[1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+function chunk(arr, size) {}
+
+module.exports = chunk;
---
+++ chunk/test.js
@@ -0,0 +1,20 @@
+const chunk = require('./index');
+
+test('array with 4 numbers, size of 2', () => {
+  expect(chunk([1, 2, 3, 4], 2)).toBe([
+    [1, 2],
+    [3, 4]
+  ]);
+});
+
+test('array with 5 numbers, size of 2', () => {
+  expect(chunk([1, 2, 3, 4, 5], 2)).toBe([[1, 2], [3, 4], [5]]);
+});
+
+test('array with 8 numbers, size of 3', () => {
+  expect(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)).toBe([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8]
+  ]);
+});
