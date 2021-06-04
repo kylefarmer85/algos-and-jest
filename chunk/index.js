afa827d8c61af163869668029c615e1fcd82592f
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-03 20:27:56 -0500

    chunk solution

--- chunk/index.js
+++ chunk/index.js
@@ -5,6 +5,17 @@
 // chunk([1, 2, 3, 4, 5], 2) --> [[1, 2], [3, 4], [5]]
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[1, 2, 3], [4, 5, 6], [7, 8]]
 
-function chunk(arr, size) {}
+function chunk(arr, size) {
+  let chunkedArr = []
+  for (let i = 0; i < arr.length; i++) {
+    let lastElem = chunkedArr[chunkedArr.length - 1]
+    if (!lastElem || lastElem.length === size) {
+      chunkedArr.push([arr[i]]);
+    } else {
+      lastElem.push(arr[i])
+    }
+  }
+  return chunkedArr
+}
 
 module.exports = chunk;
--- chunk/test.js
+++ chunk/test.js
@@ -1,18 +1,18 @@
 const chunk = require('./index');
 
 test('array with 4 numbers, size of 2', () => {
-  expect(chunk([1, 2, 3, 4], 2)).toBe([
+  expect(chunk([1, 2, 3, 4], 2)).toEqual([
     [1, 2],
     [3, 4]
   ]);
 });
 
 test('array with 5 numbers, size of 2', () => {
-  expect(chunk([1, 2, 3, 4, 5], 2)).toBe([[1, 2], [3, 4], [5]]);
+  expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
 });
 
 test('array with 8 numbers, size of 3', () => {
-  expect(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)).toBe([
+  expect(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)).toEqual([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8]
