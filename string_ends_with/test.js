027549f21e4e9f058ba3d28f216ae0c9823ffa22
kylefarmer85 <kylefarmer85@gmail.com>
2021-06-11 18:41:07 -0500

    create string_ends_with index and test

---
+++ string_ends_with/test.js
@@ -0,0 +1,17 @@
+const stringEndsWith = require('./index.js');
+
+test('returns true', () => {
+  expect(stringEndsWith('abc', 'bc')).toBe(true);
+});
+
+test('returns false', () => {
+  expect(stringEndsWith('abc', 'd')).toBe(false);
+});
+
+test('returns true', () => {
+  expect(stringEndsWith('abcde', 'cde')).toBe(true);
+});
+
+test('returns false', () => {
+  expect(stringEndsWith('abcde', 'abc')).toBe(false);
+});
